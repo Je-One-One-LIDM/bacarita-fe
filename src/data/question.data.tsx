ab0da0f9923d81e20ebedc3f5aafbfc250cc952e
mahesgya <anargya115@gmail.com>
2025-09-10 11:41:36 +0700

    feat: refactor data dummy

---
+++ src/data/question.data.tsx
@@ -0,0 +1,19 @@
+export const questionsData = {
+  "Kura-kura Bijak": [
+    {
+      id: 1,
+      type: "reading",
+      instruction: "Bacakan kalimat ini dengan suara yang jelas:",
+      text: "Lambat tapi pasti lebih baik daripada cepat tapi sombong",
+      expectedDuration: 8,
+    },
+    {
+      id: 2,
+      type: "comprehension", 
+      instruction: "Jawab pertanyaan ini dengan suara:",
+      question: "Siapa yang memenangkan lomba lari dalam cerita?",
+      expectedAnswer: ["kura-kura", "wijaya", "kura kura"],
+      textReference: "Kura-kura memenangkan lomba dengan tekad dan ketekunannya"
+    }
+  ]
+};
