5c1a979b86be00ddc4865a1f2dee27348e5f1d00
mahesgya <anargya115@gmail.com>
2025-10-25 00:19:24 +0700

    feat: make register guru payload types

--- src/types/auth.types.tsx
+++ src/types/auth.types.tsx
@@ -1,3 +1,5 @@
+//LOGIN PAYLOAD TYPES
+
 export interface LoginSuccessPayload {
     success: true;
     statusCode: number;
@@ -7,14 +9,42 @@ export interface LoginSuccessPayload {
     };
 }
 
-export interface LoginFailurePayload {
+export interface AuthFailurePayload {
     success: false;
     statusCode: number;
     error: string;
 }
 
-export interface LoginFailurePayloadValidation extends LoginFailurePayload {
+export interface AuthFailurePayloadValidation extends AuthFailurePayload {
     errors: string[];
 }
 
-export type LoginResponse = LoginSuccessPayload | LoginFailurePayload | LoginFailurePayloadValidation;
+export type LoginResponse = LoginSuccessPayload | AuthFailurePayload | AuthFailurePayloadValidation;
+
+//REGISTER PAYLOAD TYPES
+
+export type RegisterGuruPayload = {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  fullName: string;
+  schoolName: string;
+};
+
+export interface RegisterSuccessPayload {
+    success: true;
+    statusCode: number;
+    message: string;
+    data: {
+        id: string;
+        email: string;
+        username: string;
+        fullName: string;
+        schoolName: string;
+        createdAt: string;
+        updatedAt: string;
+    }
+}
+
+export type RegisterResponse = RegisterSuccessPayload | AuthFailurePayload | AuthFailurePayloadValidation;
