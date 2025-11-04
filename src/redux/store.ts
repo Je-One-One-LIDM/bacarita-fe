22a73facdea221011052be117437d5a601424321
mahesgya <anargya115@gmail.com>
2025-11-04 11:12:05 +0700

    feat: setup redux persist for test session

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"
+
 import authReducer from "./auth.slice";
 import generalReducer from "./general.slice";
+import sessionReducer from "./session.slice";
+
+const rootReducer = combineReducers({
+    auth : authReducer,
+    general: generalReducer,
+    testSession: sessionReducer,
+})
+
+const persistConfig = {
+    key : "root",
+    storage: storage,
+    whitelist: ["testSession"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        general: generalReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 })
 
 export default store; 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
