44f84b0d82363484132320633e785e299bc23e85
mahesgya <anargya115@gmail.com>
2025-11-11 00:27:15 +0700

    feat: declare interface for get response

--- src/types/teacher.types.tsx
+++ src/types/teacher.types.tsx
@@ -77,3 +77,112 @@ export interface RegisterStudentSuccessPayloadWithParent extends SuccessPayload 
 } 
 
 export type RegisterStudentResponse = RegisterStudentSuccessPayload | RegisterStudentSuccessPayloadWithParent | ErrorPayload;
+
+// OVERVIEW PAYLOAD
+
+export interface SttWordResult {
+    id: string;
+    intruction: string | null;
+    expectedWord: string;
+    spokenWord: string;
+    accuracy: number;
+    createdAt: string;
+}
+
+export interface StudentData {
+    id: string;
+    username: string;
+    fullName: string;
+}
+
+export interface TestSessionResult {
+    id: string;
+    student: StudentData;
+    levelFullName: string;
+    titleAtTaken: string;
+    startedAt: string;
+    finishedAt: string;
+    medal: string;
+    score: number;
+    isCompleted: boolean;
+    sttWordResults: SttWordResult[];
+}
+
+export interface OverviewData {
+    totalStudents: number;
+    totalTestSessions: number;
+    completedTestSessions: number;
+    inProgressTestSessions: number;
+    averageScore: number;
+    testSessions: TestSessionResult[];
+}
+
+export interface TeacherOverviewSuccessPayload extends SuccessPayload {
+    data: OverviewData;
+}
+
+export type TeacherOverviewResponse = TeacherOverviewSuccessPayload | ErrorPayload;
+
+//ALL STUDENT PAYLOAD
+
+export interface ParentPayload {
+    id: string;
+    email: string;
+    username:string;
+    fullName: string;
+}
+
+export interface LevelProgressPayload{
+    levelId: number;
+    levelNo: number;
+    levelName: string;
+    levelFullName: string;
+    isUnlocked: boolean;
+    isCompleted: boolean;
+    isSkipped: boolean;
+    currentPoints: number;
+    maxPoints: number;
+    progress: number | null;
+    requiredPoints: number;
+    goldCount: number;
+    silverCount: number;
+    bronzeCount: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface StudentData {
+    id: string;
+    username: string;
+    fullName: string;
+    parent: ParentPayload;
+    totalTestSessions: number;
+    completedTestSessions: number;
+    inProgressTestSessions: number;
+    averageScore: number;
+    lastTestSessionAt: string | null;
+    levelProgresses: LevelProgressPayload[]
+    createdAt: string;
+}
+
+export interface GetAllStudentSuccessPayload extends SuccessPayload{
+    data: StudentData[]
+}
+
+export type GetAllStudentResponse = GetAllStudentSuccessPayload | ErrorPayload
+
+//TEST SESSION OF STUDENTS PAYLOAD
+
+export interface TestSessionOfStudentSuccessPayload extends SuccessPayload {
+    data : TestSessionResult[];
+}
+
+export type TestSessionOfStudentResponse = TestSessionOfStudentSuccessPayload | ErrorPayload;
+
+//TEST SESSION SINGLE STUDENT PAYLOAD
+
+export interface TestSessionSingleStudentSuccessPayload extends SuccessPayload {
+    data : TestSessionResult;
+}
+
+export type TestSessionSingleStudentResponse = TestSessionSingleStudentSuccessPayload | ErrorPayload;
