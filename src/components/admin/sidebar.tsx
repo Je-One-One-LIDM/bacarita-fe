ddb4f9fda86e970c498477f03b7e4f737b289d68
mahesgya <anargya115@gmail.com>
2025-11-29 22:27:35 +0700

    feat: implement logout and get profile at sidebar

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/cn";
 import { User2, X } from "lucide-react";
-import { showToastSuccess } from "../utils/toast.utils";
+import { showToastError, showToastSuccess } from "../utils/toast.utils";
 import { useRouter } from "next/navigation";
 import { ProfileCard } from "../ui/profile.card";
-import { BaseProfilePayload } from "@/types/auth.types";
-
+import { AdminProfilePayload } from "@/types/auth.types";
+import LogoutServices from "@/services/logout.services";
+import { useDispatch } from "react-redux";
+import AuthServices from "@/services/auth.services";
 export type NavItem = { label: string; href: string; icon?: JSX.Element };
 
 const navItems: NavItem[] = [
@@ -22,21 +24,30 @@ type SidebarProps = { open: boolean; onClose: () => void; onToggle: () => void }
 const Sidebar: FC<SidebarProps> = ({ open, onClose, onToggle }) => {
   const pathname = usePathname();
   const router = useRouter();
+  const dispatch = useDispatch();
   const [panelOpen, setPanelOpen] = useState(false);
+  const [profile, setProfile] = useState<AdminProfilePayload>();
   const panelRef = useRef<HTMLDivElement | null>(null);
 
   const handleLogout = async () => {
+    await LogoutServices.LogoutAdmin(dispatch);
     router.push("/");
     showToastSuccess("Logout Berhasil!");
   };
 
-  const profile: BaseProfilePayload = {
-    id: "admin_123",
-    username: "Admin",
-    fullName: "Admin Bacarita",
-    createdAt: "2025-01-15T10:30:00.000Z",
-    updatedAt: "2025-02-01T14:45:00.000Z",
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseProfile = await AuthServices.GetProfileAdmin(dispatch);
+
+      if (responseProfile.success) {
+        setProfile(responseProfile.data);
+      } else {
+        showToastError(responseProfile.error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const onKey = (e: KeyboardEvent) => {
